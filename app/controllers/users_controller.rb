dc7120fc436922d20fefa05a2f2fbbc22fa18837
Raychel-310 <wakimoto.reina.infratop@gmail.com>
2023-11-06 15:04:41 +0000

    22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images 
+    #@post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def index
