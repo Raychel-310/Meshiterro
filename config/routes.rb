5b14ef4b9e21f3b9f2bf24ccd9371a9f1b356ed4
Raychel-310 <wakimoto.reina.infratop@gmail.com>
2023-11-06 14:47:43 +0000

    18

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit, :update]
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
   devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+  
   # get 'homes/top'
   root to: "homes#top"
   get 'homes/about' => "homes#about"
