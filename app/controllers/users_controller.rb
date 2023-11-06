fd5b0cdd92b677c87129073d24785de97a9e982d
Raychel-310 <wakimoto.reina.infratop@gmail.com>
2023-11-06 14:12:01 +0000

    15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images 
   end
 
   def index
+  end
+  
+  def edit
   end
 end
