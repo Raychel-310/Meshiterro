e4fd8b46d65dbbd4fd5cada7ed1ae8a675fac775
Raychel-310 <wakimoto.reina.infratop@gmail.com>
2023-11-06 14:53:41 +0000

    19

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   
